59ac5314b00da17689b3e24a325d756a692f94bb
Garret Raziel <boloomka@gmail.com>
2011-10-25 20:14:17 +0200

    nevim, jak budu s tim listem pracovat

---
+++ ogs_list.c
@@ -0,0 +1,6 @@
+#include "ogs_list.h"
+
+int ogs_list_init()
+{
+  return 0;
+}
--- ogs_list.h
+++ ogs_list.h
@@ -1,6 +1,9 @@
 #ifndef __OGS_LIST_H__
 #define __OGS_LIST_H__
 
+#include <stdio.h>
 #include "ogs_defines.h"
+
+int ogs_list_init();
 
 #endif
