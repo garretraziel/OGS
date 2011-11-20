f298c7e75640cc19811d37c3a0d6947bc48bfb01
Garret Raziel <boloomka@gmail.com>
2011-11-20 22:24:12 +0100

    pridan zaklad jedne funkce do objects

--- ogs_objects.c
+++ ogs_objects.c
@@ -11,3 +11,9 @@ int ogs_delete_object(void *pobject, int type)
     // take care of deletion for each object
     return 0;
 }
+
+int ogs_execute_object(void *pobject, int type)
+{
+    // execute function for that object
+    return 0;
+}
