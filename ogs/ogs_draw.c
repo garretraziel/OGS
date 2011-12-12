9987ea1b35eed35d72faceb9123c976e7f70cc73
Garret Raziel <boloomka@gmail.com>
2011-12-12 21:04:38 +0100

    odsazeni...

--- ogs/ogs_draw.c
+++ ogs/ogs_draw.c
@@ -16,46 +16,46 @@ int ogs_draw_element(OGS_PSCREEN screen, int type, void *element)
 {
     switch(type) {
     case OGS_WINDOW: {
-    	// postarat se o vykresleni vsech vnitrnich prvku
-    	OGS_PWINDOW_S window = (OGS_PWINDOW_S) element;
-    	ogs_i_draw_window(window, screen);
-
-	OGS_LIST_PITEM item = window -> items -> top;
-	while (item != NULL) {
-	    ogs_draw_element(screen, item -> type, item -> item);
-	    item = item -> next;
-	}
-	
-    	break;
+        // postarat se o vykresleni vsech vnitrnich prvku
+        OGS_PWINDOW_S window = (OGS_PWINDOW_S) element;
+        ogs_i_draw_window(window, screen);
+        
+        OGS_LIST_PITEM item = window -> items -> top;
+        while (item != NULL) {
+            ogs_draw_element(screen, item -> type, item -> item);
+            item = item -> next;
+        }
+        
+        break;
     }
     case OGS_BUTTON: {
-    	OGS_PBUTTON_S button = (OGS_PBUTTON_S) element;
-    	ogs_i_draw_button(button, screen);
-    	break;
+        OGS_PBUTTON_S button = (OGS_PBUTTON_S) element;
+        ogs_i_draw_button(button, screen);
+        break;
     }
     case OGS_INPUTF: {
-    	OGS_PINPUTF_S inputfield = (OGS_PINPUTF_S) element;
-    	ogs_i_draw_inputf(inputfield, screen);
-    	break;
+        OGS_PINPUTF_S inputfield = (OGS_PINPUTF_S) element;
+        ogs_i_draw_inputf(inputfield, screen);
+        break;
     }
     case OGS_INFO: {
-    	OGS_PINFO_S info = (OGS_PINFO_S) element;
-    	ogs_i_draw_info(info, screen);
-    	break;
+        OGS_PINFO_S info = (OGS_PINFO_S) element;
+        ogs_i_draw_info(info, screen);
+        break;
     }
     case OGS_TEXTARRAY: {
-    	OGS_PTEXTARRAY_S textarray = (OGS_PTEXTARRAY_S) element;
-    	ogs_i_draw_textarray(textarray, screen);
-    	break;
+        OGS_PTEXTARRAY_S textarray = (OGS_PTEXTARRAY_S) element;
+        ogs_i_draw_textarray(textarray, screen);
+        break;
     }
     case OGS_PICTURE: {
-    	OGS_PPICTURE_S picture = (OGS_PPICTURE_S) element;
-    	ogs_i_draw_picture(picture, screen);
-    	break;
+        OGS_PPICTURE_S picture = (OGS_PPICTURE_S) element;
+        ogs_i_draw_picture(picture, screen);
+        break;
     }
     default: {
-    	fprintf(stderr,"Error: unknown type for displaying, %d\n", type);
-    	break;
+        fprintf(stderr,"Error: unknown type for displaying, %d\n", type);
+        break;
     }
     }
     
@@ -65,15 +65,15 @@ int ogs_draw_element(OGS_PSCREEN screen, int type, void *element)
 int ogs_i_draw_window(OGS_PWINDOW_S window, OGS_PSCREEN screen)
 {
     if (window -> fullscreen == OGS_FULLSCREEN) {
-	boxColor(screen -> screen, 0, 0, screen -> resolution.width, screen -> resolution.height,\
-		 WINDOW_C);
+        boxColor(screen -> screen, 0, 0, screen -> resolution.width, screen -> resolution.height, \
+                 WINDOW_C);
     } else {
-	boxColor(screen -> screen, window -> position.width, window -> position.height,	\
-		 window -> position.width + window -> size.width, window -> position.height + \
-		 window -> size.height, WINDOW_C);
-	rectangleColor(screen -> screen, window -> position.width+2, window -> position.height +2,	\
-		 window -> position.width + window -> size.width - 2, window -> position.height + \
-		 window -> size.height - 2, WHITE);
+        boxColor(screen -> screen, window -> position.width, window -> position.height, \
+                 window -> position.width + window -> size.width, window -> position.height + \
+                 window -> size.height, WINDOW_C);
+        rectangleColor(screen -> screen, window -> position.width+2, window -> position.height +2, \
+                       window -> position.width + window -> size.width - 2, window -> position.height + \
+                       window -> size.height - 2, WHITE);
     }
     
     return 0;
@@ -82,17 +82,17 @@ int ogs_i_draw_window(OGS_PWINDOW_S window, OGS_PSCREEN screen)
 int ogs_i_draw_button(OGS_PBUTTON_S button, OGS_PSCREEN screen)
 {
     roundedBoxColor(screen -> screen, button->position.width-3, button->position.height-3, \
-		    button -> position.width + button -> size.width + 3, \
-		    button -> position.height + button -> size.height + 3, 2, 0xaaaaaaff); //TODO: asi ne pomoci alfakanalu
+                    button -> position.width + button -> size.width + 3, \
+                    button -> position.height + button -> size.height + 3, 2, 0xaaaaaaff); //TODO: asi ne pomoci alfakanalu
     if (button -> enabled) {
-	roundedBoxColor(screen -> screen, button->position.width, button->position.height, \
-		 button -> position.width + button -> size.width,	\
-			button -> position.height + button -> size.height, 2, WHITE);
-	//TODO: jen pokus, udělat jinak
+        roundedBoxColor(screen -> screen, button->position.width, button->position.height, \
+                        button -> position.width + button -> size.width, \
+                        button -> position.height + button -> size.height, 2, WHITE);
+        //TODO: jen pokus, udělat jinak
     }
-    stringColor(screen -> screen, button -> position.width + 10,	\
-		button -> position.height + button -> size.height/2,	\
-		button -> caption, 0x000000ff);
+    stringColor(screen -> screen, button -> position.width + 10,        \
+                button -> position.height + button -> size.height/2,    \
+                button -> caption, 0x000000ff);
     //SDL_UpdateRect(screen ->  screen, 0, 0, 0, 0);
     return 0;
 }
@@ -116,7 +116,7 @@ int ogs_i_draw_picture(OGS_PPICTURE_S picture, OGS_PSCREEN screen)
 {
     SDL_Rect rcDest = {picture -> position.width, picture -> position.height, 0, 0}; //TODO: velikost!!
     //TODO: resit rozmery, vyrez vs. zmenseni a tak
-
+    
     SDL_BlitSurface(picture -> image, NULL, screen -> screen, &rcDest);
     
     return 0;
@@ -125,10 +125,10 @@ int ogs_i_draw_picture(OGS_PPICTURE_S picture, OGS_PSCREEN screen)
 int ogs_redraw(OGS_PSCREEN screen)
 {
     OGS_LIST_PITEM item = screen -> list -> top;
-
+    
     while (item != NULL) {
-	ogs_draw_element(screen, item -> type, item -> item);
-	item = item -> next;
+        ogs_draw_element(screen, item -> type, item -> item);
+        item = item -> next;
     }
     
     SDL_UpdateRect(screen -> screen, 0, 0, 0, 0);
