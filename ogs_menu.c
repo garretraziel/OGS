9607bdba043c65c814d9272a489b701dd9347a99
Garret Raziel <boloomka@gmail.com>
2012-01-07 18:04:47 +0100

    zkouska tlacitek

--- ogs_menu.c
+++ ogs_menu.c
@@ -40,14 +40,22 @@ int main(int argc, char *argv[])
     
     // code:
     
-    OGS_PWINDOW_S window = ogs_create_window(0, 0, 700, 500, OGS_REL, OGS_WINDOWED);
+    OGS_PWINDOW_S window = ogs_create_window(0, 0, 700, 500, OGS_REL, OGS_FULLSCREEN);
     ogs_add_button_to_window(window, 50, 50, 200, 80, "Hello, world", pozdrav, &arg, 1);
     ogs_add_button_to_window(window, 50, 100, 200, 130, "Herp derp", pozdrav2, NULL, 1);
     ogs_add_button_to_window(window, 300, 50, 450, 80, "Hello, world2", pozdrav3, &arg2, 1);
     ogs_add_button_to_window(window, 300, 100, 450, 130, "Herp derp2", pozdrav4, NULL, 1);
     ogs_add_button_to_window(window, 550, 50, 700, 80, "Hello, world3", pozdrav3, &arg2, 1);
     ogs_add_button_to_window(window, 550, 100, 700, 130, "Herp derp3", pridej_tlacitko, &souradnice, 1);
-    ogs_add_picture_to_window(window, 50, 200, 550, 500, "sekera.png");
+
+    ogs_add_button_to_window(window, 70, 150, 220, 180, "Hello, world4", pozdrav, &arg, 1);
+    ogs_add_button_to_window(window, 240, 150, 390, 180, "Herp derp5", pozdrav2, NULL, 1);
+    ogs_add_button_to_window(window, 410, 150, 560, 180, "Hello, world25", pozdrav3, &arg2, 1);
+    ogs_add_button_to_window(window, 580, 150, 730, 180, "Herp derp25", pozdrav4, NULL, 1);
+    ogs_add_button_to_window(window, 50, 200, 200, 230, "Hello, world35", pozdrav3, &arg2, 1);
+    ogs_add_button_to_window(window, 300, 200, 450, 230, "Herp derp35", pridej_tlacitko, &souradnice, 1);
+    
+    //ogs_add_picture_to_window(window, 50, 200, 550, 500, "sekera.png");
     ogs_add_window(window, screen);
       
     int cont = OGS_NOEVENT;
